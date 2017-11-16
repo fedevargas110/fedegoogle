a19a3af89b0dd1dadd631532791fc08da375a9f6
Fede Vargas <fedevargas110@gmail.com>
2017-11-16 14:11:06 -0300

    corregida la sideNav

--- js/javas.js
+++ js/javas.js
@@ -1,3 +1,18 @@
+function initializeFirebase() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBGGI2y7_Vcp_guzHuUZAI-e3Bbthh6nEk",
+    authDomain: "project-5049986903013159327.firebaseapp.com",
+    databaseURL: "https://project-5049986903013159327.firebaseio.com",
+    projectId: "project-5049986903013159327",
+    storageBucket: ""
+  };
+  firebase.initializeApp(config);
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+}
+
+initializeFirebase();
+
 document.addEventListener("keydown", function(evento) {
   if (evento.keyCode == 13) {
     linker();
@@ -7,10 +22,10 @@ document.addEventListener("keydown", function(evento) {
 
 /*
 function linker(){
-    if(document.getElementById("barra").value!= ""){
+if(document.getElementById("barra").value!= ""){
 
-        window.location.href = "https://www.google.com.ar/search?q="+document.getElementById("barra").value+"&oq=yugioh&gs_l=psy-ab.3..0i67k1j0i131k1j0l2.65229.67815.0.68113.9.9.0.0.0.0.108.777.5j3.9.0.dummy_maps_web_fallback.2..0...1.1.64.psy-ab..0.9.865.6..35i39k1j0i10i67k1.95.JDmYu3VhW1s";
-    }
+window.location.href = "https://www.google.com.ar/search?q="+document.getElementById("barra").value+"&oq=yugioh&gs_l=psy-ab.3..0i67k1j0i131k1j0l2.65229.67815.0.68113.9.9.0.0.0.0.108.777.5j3.9.0.dummy_maps_web_fallback.2..0...1.1.64.psy-ab..0.9.865.6..35i39k1j0i10i67k1.95.JDmYu3VhW1s";
+}
 
 }
 */
@@ -22,21 +37,17 @@ function cambiar2() {
 }
 
 /*function linker2(){
-    window.location.href = "https://es.wikipedia.org/wiki/"+document.getElementById("barra").value;
+window.location.href = "https://es.wikipedia.org/wiki/"+document.getElementById("barra").value;
 }*/
 
 function Signingoogle() {
   var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('profile');
+  provider.addScope('email');
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-    var photo = user.photoURL;
-    document.getElementById('usr_img').src = photo;
 
-    writeUserData();
 
 
   }).catch(function(error) {
@@ -52,26 +63,22 @@ function Signingoogle() {
 
 }
 
-function initializeFirebase() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBGGI2y7_Vcp_guzHuUZAI-e3Bbthh6nEk",
-    authDomain: "project-5049986903013159327.firebaseapp.com",
-    databaseURL: "https://project-5049986903013159327.firebaseio.com",
-    projectId: "project-5049986903013159327",
-    storageBucket: ""
-  };
-  firebase.initializeApp(config);
-}
-
-initializeFirebase();
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var photo = user.photoURL;
+    document.getElementById('usr_img').src = photo;
+    writeUserData();
+    sideNavbar();
+  } else {
+    // Sign-out successful.
+    let img = $("#usr_img");
+    img.attr("src", "https://www.materialui.co/materialIcons/action/account_circle_grey_192x192.png");
+  }
+});
 
 function salir() {
   console.log("Adasd");
   firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-    let img = $("#usr_img");
-    img.attr("src", "https://www.materialui.co/materialIcons/action/account_circle_grey_192x192.png");
   }).catch(function(error) {
     // An error happened.
   });
@@ -80,8 +87,6 @@ function salir() {
 function mostrarmapa() {
   $("#container").load("map.html");
 }
-
-
 
 (function() {
   var cx = '005853302836175793985:e2fzujjywhk';
@@ -121,5 +126,19 @@ function updateData() {
 }
 
 $( document ).ready(function(){
-   $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav();
 })
+
+function sideNavbar(){
+
+  var user = firebase.auth().currentUser;
+  var user_nav = user.photoURL;
+  var name_nav = user.displayName;
+  var email_nav = user.email;
+
+  $("#imgG").attr("src", user_nav);
+  $("#nameG").empty();
+  $("#nameG").text(name_nav);
+  $("#emailG").empty();
+  $("#emailG").text(email_nav);
+}
